01d0145ea586242de004b6a6c91f410dfda4168c
ViniciusAP13 <viniciusamarop@gmail.com>
2025-08-19 08:53:25 -0300

    atuazisacoes

---
+++ homepage.js
@@ -0,0 +1,63 @@
+//importa as funções necessárias do firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, GoogleProvider, signOut, onAuthStateChaged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+//configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAG5nEkX01f3jIoQl-03JOcUU24oGZOdac",
+    authDomain: "openidconnect-9769b.firebaseapp.com",
+    projectId: "openidconnect-9769b",
+    storageBucket: "openidconnect-9769b.firebasestorage.app",
+    messagingSenderId: "190720060731",
+    appId: "1:190720060731:web:c663182f2f6867267c9b1e"
+  };
+
+
+//inicializa o firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(); //configura o firebase authentication
+const db = getFirestore(); //configura o firestore
+
+//monitora o estado de autenticação do usuário
+onAuthStateChanged(auth, (user) => {
+    //busca o id do usuário autenticado salvo no localStorage
+    const loggedInUserId = localStorage.getItem('loggedInUsarId');
+
+    //se o ID estiver no localStorage, tenta obter os dados do Firestore
+    if (loggedInUserId) {
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId); //referência ao documento do usuário no firestore
+
+        getDoc(docRef) //Busca o documento
+        .then((docSnap) => {
+            //se o documento existir, exibe os dados na interface
+            if (docSnap.exists()) {
+                const userData = docSnap.data();
+                document.getElementById('loggedUserFName').innerText = userData.firstName;
+                document.getElementById('loggedUserEmail').innerText = userData.email;
+                document.getElementById('loggedUserLName').innerText = userData.lastName;
+            } else {
+                console.log("ID não encontrado no Documento");
+            }
+        })
+        .catch((error) => {
+            console.log("documento não encontrado");
+        });
+    } else {
+        console.log("ID de usuário não encontrado no localStorage");
+    }
+});
+
+//Lógica de Logout
+const logoutButton = document.getElementById('logout');
+logoutButton.addEventListener('click', () => {
+    localStorage.removeItem('loggedInUserId'); //remove o ID do LocalStorage
+    signOut(auth) //realiza logout
+    .then(() => {
+        window.location.href = 'index.html'; // redireciona para a página de login
+    })
+    .catch((error) => {
+        console.error('Error Signing out:', error);
+    });
+});
